659f48b4787d2e9e9669e2e9e8b8757aa977f0a4
vishakhviswan <vishakhviswan1995@gmail.com>
2021-10-08 20:24:36 +0530

    On Work

--- main.py
+++ main.py
@@ -15,16 +15,55 @@ def button_click(number):
 def button_add():
     first_num = e.get()
     global f_num
+    global math
+    math = "addition"
     f_num = int(first_num)
     e.delete(0,END)
 def button_equal():
     second_num = e.get()
     e.delete(0,END)
-    e.insert(0,f_num +int(second_num))
 
+    if math == "addition":
+        e.insert(0, f_num + int(second_num))
+    if math == "subtraction":
+        e.insert(0, f_num - int(second_num))
+    if math == "multiplication":
+        e.insert(0, f_num * int(second_num))
+    if math == "division":
+        e.insert(0, f_num / int(second_num))
 
+def button_subtract():
+    first_num = e.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_num)
+    e.delete(0, END)
+
+def button_multiply():
+    first_num = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_num)
+    e.delete(0, END)
 def button_ac():
     e.delete(0,END)
+
+def button_divide():
+    first_num = e.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_num)
+    e.delete(0, END)
+def button_percentage():
+   second_number = e.get()
+   e.delete(0,END)
+   if math =="multiplication":
+       e.insert(0, f_num / 100 * int(second_number))
+   if math =="division":
+       e.insert(0, f_num * 100 / int(second_number))
 
 def button_clear():
     e.delete(0,END)
@@ -33,9 +72,9 @@ def button_clear():
 button_ac = Button(window,text="AC",padx=41, pady=20,command = button_ac )
 button_c = Button(window,text="C",padx=46, pady=20,command = button_clear)
 button_pon = Button(window,text="+/-",padx=40, pady=20,command =button_add )
-button_div = Button(window,text="/",padx=40, pady=20,command = button_add )
-button_mul = Button(window,text="X",padx=40, pady=20,command = button_add )
-button_min = Button(window,text="-",padx=40, pady=20,command = button_add )
+button_div = Button(window,text="/",padx=40, pady=20,command = button_divide )
+button_mul = Button(window,text="X",padx=40, pady=20,command = button_multiply )
+button_min = Button(window,text="-",padx=40, pady=20,command = button_subtract )
 button_plus = Button(window,text="+",padx=40, pady=20,command = button_add )
 button_equal = Button(window,text="=",padx=40, pady=20,command = button_equal)
 
@@ -48,9 +87,9 @@ button_6 = Button(window,text="6",padx=46, pady=20,command = lambda:button_click
 button_1 = Button(window,text="1",padx=46, pady=20,command = lambda:button_click(1) )
 button_2 = Button(window,text="2",padx=46, pady=20,command = lambda:button_click(2) )
 button_3 = Button(window,text="3",padx=46, pady=20,command = lambda:button_click(3) )
-button_per = Button(window,text="%",padx=45, pady=20,command = lambda:button_click() )
-button_0 = Button(window,text="0",padx=46, pady=20,command = lambda:button_click() )
-button_dot = Button(window,text=".",padx=47, pady=20,command = lambda:button_click() )
+button_per = Button(window,text="%",padx=45, pady=20,command =button_percentage)
+button_0 = Button(window,text="0",padx=46, pady=20,command = lambda:button_click(0) )
+button_dot = Button(window,text=".",padx=47, pady=20,command = lambda:button_click(".") )
 
 # Put Buttons on the screen
 button_ac.grid(row=1,column=0)
